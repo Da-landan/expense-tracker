e96fcd09161f59d15d11f483b79855790e628b2f
Lance <lancedaniel.g4122@gmail.com>
2026-09-08 00:01:21 +0800

    Added the /auth/login API

--- src/main/java/com/dalandan/expense_tracker/controller/AuthController.java
+++ src/main/java/com/dalandan/expense_tracker/controller/AuthController.java
@@ -1,11 +1,15 @@
 package com.dalandan.expense_tracker.controller;
 
+import com.dalandan.expense_tracker.dto.LoginUserRequest;
 import com.dalandan.expense_tracker.dto.RegisterUserRequest;
 import com.dalandan.expense_tracker.model.User;
+import com.dalandan.expense_tracker.repository.UserRepository;
+import com.dalandan.expense_tracker.security.JwtUtil;
 import com.dalandan.expense_tracker.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,9 +17,20 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final UserService userService;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
-    public AuthController(UserService userService) {
+    public AuthController(
+            UserService userService,
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder,
+            JwtUtil jwtUtil
+    ) {
         this.userService = userService;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
     }
 
     @PostMapping("/register")
@@ -31,5 +46,36 @@ public class AuthController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(
+            @Valid @RequestBody LoginUserRequest request
+    ) {
+
+        User user = userRepository
+                .findByUsername(request.getUsername())
+                .orElse(null);
+
+        if (user == null) {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .build();
+        }
+
+        boolean passwordMatches = passwordEncoder.matches(
+                request.getPassword(),
+                user.getPassword()
+        );
+
+        if (!passwordMatches) {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .build();
+        }
+
+        String token = jwtUtil.generateToken(user.getUsername());
+
+        return ResponseEntity.ok(token);
     }
 }
